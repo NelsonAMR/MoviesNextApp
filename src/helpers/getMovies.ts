cf9670297ef207e062fdc53ba1e8e10ea1eef3ef
NelsonAMR <nelson.macias99@gmail.com>
2023-07-07 13:40:13 -0600

    create a index at /src/helpers to export easier

--- src/helpers/getMovies.ts
+++ src/helpers/getMovies.ts
@@ -1,4 +1,4 @@
-async function getMovies() {
+export async function getMovies() {
   const { API_URL, API_KEY } = process.env;
   const response = await fetch(
     `${API_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
