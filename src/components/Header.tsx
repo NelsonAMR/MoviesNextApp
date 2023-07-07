09b87dbe1ffa6fe4345f348d3c2907f3d8bf9608
NelsonAMR <nelson.macias99@gmail.com>
2023-07-07 11:38:41 -0600

    change some styles in Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,11 @@ export function Header() {
   return (
     <header className="bg-primary h-14">
       <div className="container flex justify-between items-center mx-auto h-full gap-10">
-        <h1 className="text-2xl font-bold text-secondary">LOGO</h1>
+        <h1 className="text-2xl font-bold text-secondary">MOVIES</h1>
         <input
           type="text"
-          className="rounded p-1 outline-none w-full max-w-lg"
+          className="rounded p-1 pl-2 outline-none w-full max-w-lg"
+          placeholder="Search a movie..."
         />
       </div>
     </header>
